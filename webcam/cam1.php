b15e6a34a9e33b9c78f13aa4182e05d05fec0a4f
Alex <87544948+achaglov@users.noreply.github.com>
2022-04-27 12:57:12 +0500

    Update cam1.php

--- webcam/cam1.php
+++ webcam/cam1.php
@@ -2,5 +2,5 @@
 //set_time_limit(0);
 header('Content-Type: image/jpeg');
 //header('Content-Type: multipart/x-mixed-replace;boundary=ipcamera'); 
-readfile("http://admin:w7cdmtq@192.168.1.50:5914/snapshot.cgi");
+readfile("http://user:password@IP:port/snapshot.cgi");
 ?>
